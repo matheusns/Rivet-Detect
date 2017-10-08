f563b3ce30c92af079be801b3f4ab558cd635998
Matheus Nascimento <contamatheusnascimento@gmail.com>
2017-10-08 16:08:00 -0300

    Improves some parameters

--- rivetFind.py
+++ rivetFind.py
@@ -1,10 +1,11 @@
 import cv2
 import numpy as np
+from autoCanny import *
 from binaryProcess import *
 from findCircles import *
 
 #Video Capture
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 fn = "black.jpg"
 cont = 0
 while True:
@@ -13,8 +14,8 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 cv2.destroyAllWindows()
+
 while(cont!=100):
-    # print "Cont = " + str(cont)
     # Frame Capture
     ret, frame = cap.read()
     if cont==0:
@@ -27,17 +28,15 @@ while(cont!=100):
     if circles is not None:
         c = np.uint16(np.around(circles))
         for i in c[0,:]:
-            # draw the circles
-            # print "Raio = " + str(i[2])
-            # if i[2]>=17:
+            print "Raio = " + str(i[2])
+            # if i[2]>=8:
             cv2.circle(black_image,(i[0],i[1]),5,(255,255,255),12,) #Drawing the circle
-            cv2.imshow("black_image", black_image)
-            cv2.imshow("Canny", binary)
-            cv2.imshow("BRG", frame)
+            cv2.imshow("Black Image", black_image)
+            cv2.imshow("Binary Image", binary)
+            cv2.imshow("Original Frame ", frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
    
     cont+=1
-
-# cv2.destroyAllWindows()
+#Calls the find circles script
 findCircles(black_image,frame)
